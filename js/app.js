ad7c5e1908147a126053f2a1c12cce8d04a9d392
Ben Jaffe <ben@udacity.com>
2014-10-13 16:48:19 -0700

    Implemented a cat collection

--- js/app.js
+++ js/app.js
@@ -2,24 +2,56 @@ var catElem = document.getElementById('cat');
 var catNameElem = document.getElementById('cat-name');
 var catImageElem = document.getElementById('cat-img');
 var countElem = document.getElementById('cat-count');
-var cat = {
+
+var catListElem = document.getElementById('cat-list');
+
+var cats = [{
     clickCount : 0,
     name : 'Tabby',
     imgSrc : 'img/434164568_fea0ad4013_z.jpg',
     imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
-};
+},
+{
+    clickCount : 0,
+    name : 'Tiger',
+    imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+}];
+
+var currentCat = cats[0];
 
 function incrementCounter() {
-    cat.clickCount++;
-    view.render();
+    currentCat.clickCount++;
+    catView.render();
 }
 
-var view = {
+var catView = {
     render: function() {
         // update the view
-        countElem.textContent = cat.clickCount;
-        catNameElem.textContent = cat.name
-        catImageElem.src = cat.imgSrc;
+        countElem.textContent = currentCat.clickCount;
+        catNameElem.textContent = currentCat.name
+        catImageElem.src = currentCat.imgSrc;
+    }
+}
+
+var catListView = {
+    render: function() {
+        // empty cat list
+        catListElem.innerHTML = '';
+
+        for (var i = 0; i < cats.length; i++) {
+            // make a new cat list item, and append it
+            var cat = cats[i];
+            var elem = document.createElement('li');
+            elem.textContent = cat.name;
+            elem.addEventListener('click', (function(cat) {
+                return function() {
+                    currentCat = cat;
+                    catView.render();
+                };
+            })(cat))
+            catListElem.appendChild(elem);
+        };
     }
 }
 
@@ -29,4 +61,5 @@ catElem.addEventListener('click', function(e){
 });
 
 // initial render
-view.render();
+catListView.render();
+catView.render();

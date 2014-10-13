d43cca4c8ad6a8f546f9377156f1905932b6a0a1
Ben Jaffe <ben@udacity.com>
2014-10-13 16:53:01 -0700

    added three more cats

--- js/app.js
+++ js/app.js
@@ -9,13 +9,31 @@ var cats = [{
     clickCount : 0,
     name : 'Tabby',
     imgSrc : 'img/434164568_fea0ad4013_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+    imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
 },
 {
     clickCount : 0,
     name : 'Tiger',
     imgSrc : 'img/4154543904_6e2428c421_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+    imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
+},
+{
+    clickCount : 0,
+    name : 'Scaredy',
+    imgSrc : 'img/22252709_010df3379e_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+},
+{
+    clickCount : 0,
+    name : 'Shadow',
+    imgSrc : 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+},
+{
+    clickCount : 0,
+    name : 'Sleepy',
+    imgSrc : 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
 }];
 
 var currentCat = cats[0];

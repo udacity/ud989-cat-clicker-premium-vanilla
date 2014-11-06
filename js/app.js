5c39bd2a1ebd5dc8101a1627e211c105926adb5d
Ben Jaffe <ben@udacity.com>
2014-11-06 09:37:28 -0800

    finished cat clicker premium

--- js/app.js
+++ js/app.js
@@ -1,93 +1,147 @@
-var catElem = document.getElementById('cat');
-var catNameElem = document.getElementById('cat-name');
-var catImageElem = document.getElementById('cat-img');
-var countElem = document.getElementById('cat-count');
-
-var catListElem = document.getElementById('cat-list');
-
 
 /* ======= Model ======= */
 
-var cats = [{
-    clickCount : 0,
-    name : 'Tabby',
-    imgSrc : 'img/434164568_fea0ad4013_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
-},
-{
-    clickCount : 0,
-    name : 'Tiger',
-    imgSrc : 'img/4154543904_6e2428c421_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
-},
-{
-    clickCount : 0,
-    name : 'Scaredy',
-    imgSrc : 'img/22252709_010df3379e_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
-},
-{
-    clickCount : 0,
-    name : 'Shadow',
-    imgSrc : 'img/1413379559_412a540d29_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
-},
-{
-    clickCount : 0,
-    name : 'Sleepy',
-    imgSrc : 'img/9648464288_2516b35537_z.jpg',
-    imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
-}];
+var model = {
+    currentCat: null,
+    cats: [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
+        }
+    ]
+};
 
-var currentCat = cats[0];
+
+/* ======= Octopus ======= */
+
+var octopus = {
+
+    init: function() {
+        // set our current cat to the first one in the list
+        model.currentCat = model.cats[0];
+
+        // tell our views to initialize
+        catListView.init();
+        catView.init();
+    },
+
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
+
+    // increments the counter for the currently-selected cat
+    incrementCounter: function() {
+        model.currentCat.clickCount++;
+        catView.render();
+    }
+};
 
 
 /* ======= View ======= */
 
 var catView = {
+
+    init: function() {
+        // store pointers to our DOM elements for easy access later
+        this.catElem = document.getElementById('cat');
+        this.catNameElem = document.getElementById('cat-name');
+        this.catImageElem = document.getElementById('cat-img');
+        this.countElem = document.getElementById('cat-count');
+
+        // on click, increment the current cat's counter
+        this.catImageElem.addEventListener('click', function(e){
+            octopus.incrementCounter();
+        });
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
     render: function() {
-        // update the view
-        countElem.textContent = currentCat.clickCount;
-        catNameElem.textContent = currentCat.name
-        catImageElem.src = currentCat.imgSrc;
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name
+        this.catImageElem.src = currentCat.imgSrc;
     }
 }
 
 var catListView = {
-    render: function() {
-        // empty cat list
-        catListElem.innerHTML = '';
 
+    init: function() {
+        // store the DOM element for easy access later
+        this.catListElem = document.getElementById('cat-list');
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        // get the cats we'll be rendering from the octopus
+        var cats = octopus.getCats();
+
+        // empty the cat list
+        this.catListElem.innerHTML = '';
+
+        // loop over the cats
         for (var i = 0; i < cats.length; i++) {
-            // make a new cat list item, and append it
+            // this is the cat we're currently looping over
             var cat = cats[i];
+
+            // make a new cat list item and set its text
             var elem = document.createElement('li');
-            var text = cat.name;
-            elem.textContent = text;
+            elem.textContent = cat.name;
+
+            // on click, setCurrentCat and render the catView
+            // (this uses our closure-in-a-loop trick to connect the value
+            //  of the cat variable to the click event function)
             elem.addEventListener('click', (function(cat) {
                 return function() {
-                    currentCat = cat;
+                    octopus.setCurrentCat(cat);
                     catView.render();
                 };
             })(cat));
-            catListElem.appendChild(elem);
+
+            // finally, add the element to the list
+            this.catListElem.appendChild(elem);
         };
     }
 }
 
-
-/* ======= Glue ======= */
-
-function incrementCounter() {
-    currentCat.clickCount++;
-    catView.render();
-}
-
-// attach event listeners
-catElem.addEventListener('click', function(e){
-    incrementCounter();
-});
-
-// initial render
-catListView.render();
-catView.render();
+// make it go!
+octopus.init();
